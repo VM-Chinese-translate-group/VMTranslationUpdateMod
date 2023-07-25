5610cafd1a35d8bac2f7d254cd9c50264e559c8f
Wulian233 <71213467+Wulian233@users.noreply.github.com>
2023-07-25 14:07:24 +0800

    v1.4

--- src/main/java/top/vmctcn/vmtranslationupdate/VmTranslationUpdate.java
+++ src/main/java/top/vmctcn/vmtranslationupdate/VmTranslationUpdate.java
@@ -3,8 +3,7 @@ package top.vmctcn.vmtranslationupdate;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.*;
-import net.minecraft.util.text.event.ClickEvent;
-import net.minecraft.util.text.event.HoverEvent;
+import net.minecraft.util.text.event.*;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -14,19 +13,22 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 
-@Mod(modid = "vmtranslationupdate", name = "VM Chinese Group Translation Update", version = "1.2")
+@Mod(modid = "vmtranslationupdate", name = "VM Translation Update", version = "1.4")
 public class VmTranslationUpdate {
-    private Random random;
-    private int tickCounter;
-    private static int minutes;
+    public Random random;
+    public final List<String> messagesList = new ArrayList<>();
+    public int tickCounter;
+    public static int minutes;
     public static Configuration config;
-    public static String updateurl,downloadurl,version;
+    public static String updateurl,downloadurl,tipsurl,version;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
@@ -39,16 +41,15 @@ public class VmTranslationUpdate {
     @SubscribeEvent
     public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
         EntityPlayer player = event.player;
-        String localVersion = version;
         String onlineVersion = getOnlineVersion(player);
-        String name = player.getDisplayNameString();
+        String name = player.getName();
         if (name.equals("Zi__Min")) {
             name = "岷叔";
-            player.sendMessage(new TextComponentString("欢迎来到籽岷的Minecraft游戏世界！"));
+            player.sendMessage(new TextComponentString("欢迎来到籽岷的Minecraft游戏世界"));
         }
 
-        if (onlineVersion != null && !localVersion.equals(onlineVersion)) {
-            player.sendMessage(new TextComponentString("VM汉化组：你好"+name+"，当前汉化已过时，请更新你的汉化！当前版本为"+localVersion+"，最新版为"+getOnlineVersion(player)+"。"));
+        if (onlineVersion != null && !version.equals(onlineVersion)) {
+            player.sendMessage(new TextComponentString("VM汉化组：你好"+name+"，当前汉化已过时，请更新你的汉化！当前版本为"+version+"，最新版为"+getOnlineVersion(player)+"。"));
 
             ITextComponent message = new TextComponentString("请点击右侧").appendSibling(
                     new TextComponentTranslation(downloadurl)
@@ -66,15 +67,15 @@ public class VmTranslationUpdate {
     public static void syncConfig() {
         updateurl = config.get("VM汉化组汉化检测配置", "updateurl", "https://vmct-cn.top/rad/update.txt", "获取TXT检测更新的url").getString();
         downloadurl = config.get("VM汉化组汉化检测配置", "downloadurl", "https://vmct-cn.top/rad/", "提示玩家下载地址的url").getString();
+        tipsurl = config.get("VM汉化组汉化检测配置", "tipsurl", "https://vmct-cn.top/tips.txt", "获取知识内容的url").getString();
         version = config.get("VM汉化组汉化检测配置", "version", "第一版", "当前汉化版本").getString();
-        minutes = config.get("VM汉化组汉化检测配置", "minutes", 25, "发送消息的时间间隔（分钟）").getInt();
+        minutes = config.get("VM汉化组汉化检测配置", "minutes", 25, "发送知识的时间间隔（分钟）").getInt();
         if (config.hasChanged()) {
             config.save();
         }
     }
 
-
-    private String getOnlineVersion(EntityPlayer player) {
+    public String getOnlineVersion(EntityPlayer player) {
         try {
             URL url = new URL(updateurl);
             URLConnection connection = url.openConnection();
@@ -98,43 +99,41 @@ public class VmTranslationUpdate {
         if (event.phase == TickEvent.Phase.END && Minecraft.getMinecraft().world != null) {
             tickCounter++;
             int tickInterval = 20 * 60 * minutes;
-
             if (tickCounter >= tickInterval) {
                 tickCounter = 0;
-                sendRandomMessage();
+                String randomMessage = getRandomMessageFromURL(tipsurl);
+                if (randomMessage != null) {
+                    Minecraft minecraft = Minecraft.getMinecraft();
+                    if (minecraft.player != null) {
+                        minecraft.player.sendMessage(new TextComponentString(randomMessage));
+                    }
+                }
             }
         }
     }
 
+    public String getRandomMessageFromURL(String tipsurl) {
+        if (messagesList.isEmpty()) {
+            loadMessagesFromURL(tipsurl);
+        }
+        if (!messagesList.isEmpty()) {
+            int index = random.nextInt(messagesList.size());
+            return messagesList.get(index);
+        }
+        return null;
+    }
 
-    private void sendRandomMessage() {
-        Minecraft minecraft = Minecraft.getMinecraft();
-        if (minecraft.player != null) {
-            String message = getRandomMessage();
-            minecraft.player.sendMessage(new TextComponentString(message));
+    public void loadMessagesFromURL(String tipsurl) {
+        try {
+            URL url = new URL(tipsurl);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                messagesList.add(line);
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-
-    private String getRandomMessage() {
-        String[] messages = {
-                "你知道吗： 凋零是一种状态效果，凋灵是一种敌对生物。因此绝大多数情况下应称为凋灵。",
-                "你知道吗： 拧字有3个读音，向两个方向使劲叫2声拧，拧干。一个方向是3声，拧螺丝。",
-                "你知道吗： 荧石是一种发光方块，火字底。萤石是氟化钙，虫字底。因此绝大多数情况下应称为荧石。",
-                "你知道吗： 地狱在1.16后更名为下界，因此高版本都是下界。",
-                "你知道吗： 曲字有2个读音，带弯的念1声，曲线。乐曲音乐是3声，曲艺。",
-                "你知道吗： 髓有且只有一个读音！读精髓。",
-                "你知道吗： 看对联最后一个字平仄声。3声4声是上联，贴右边，别贴反啦。",
-                "你知道吗： 新版MC的菌丝已经改为菌丝体了。",
-                "你知道吗： 新版MC的速度已经改为迅捷了。",
-                "你知道吗： 新版MC的末影水晶已经改为末地水晶了。",
-                "你知道吗： 新版MC的干草块已经改为干草捆了。",
-                "你知道吗： 新版MC的摔落保护已经改为摔落缓冲了。",
-                "你知道吗： “因为”的“为”念4声。",
-                "你知道吗： 这是VM汉化组汉化更新检测模组发出的一条消息。"
-        };
-
-        int index = random.nextInt(messages.length);
-        return messages[index];
-    }
 }
-
