f5538a73d2a4f52b7fe903aea34148f8f12b64da
TexTrue <3140846162@qq.com>
2023-09-03 06:42:29 +0800

    fix

---
+++ common/src/main/java/top/vmctcn/vmtranslationupdate/util/ModConfigUtil.java
@@ -0,0 +1,24 @@
+package top.vmctcn.vmtranslationupdate.util;
+
+import me.shedaniel.architectury.platform.Platform;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
+
+import top.vmctcn.vmtranslationupdate.VMTranslationUpdate;
+import top.vmctcn.vmtranslationupdate.config.ConfigScreen;
+
+public class ModConfigUtil {
+    private static ConfigScreen configScreen;
+
+    public static ConfigScreen getConfig() {
+        if (configScreen == null) {
+            AutoConfig.register(ConfigScreen.class, Toml4jConfigSerializer::new);
+            configScreen = AutoConfig.getConfigHolder(ConfigScreen.class).getConfig();
+        }
+        return configScreen;
+    }
+
+    public static void setConfigScreen() {
+        Platform.getMod(VMTranslationUpdate.MOD_ID).registerConfigurationScreen(parent -> AutoConfig.getConfigScreen(ConfigScreen.class, parent).get());
+    }
+}
---
+++ forge/src/main/java/top/vmctcn/vmtranslationupdate/forge/VMTranslationUpdateClientForge.java
@@ -0,0 +1,28 @@
+package top.vmctcn.vmtranslationupdate.forge;
+
+import me.shedaniel.architectury.platform.forge.EventBuses;
+import me.shedaniel.architectury.utils.EnvExecutor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
+import top.vmctcn.vmtranslationupdate.VMTranslationUpdate;
+import top.vmctcn.vmtranslationupdate.util.ModConfigUtil;
+
+@Mod(VMTranslationUpdate.MOD_ID)
+public class VMTranslationUpdateClientForge {
+    public VMTranslationUpdateClientForge() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        EventBuses.registerModEventBus(VMTranslationUpdate.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+
+        EnvExecutor.runInEnv(Dist.CLIENT, () -> this::onInitializeClient);
+    }
+
+    public void onInitializeClient() {
+        VMTranslationUpdate.init();
+        ModConfigUtil.setConfigScreen();
+    }
+}
