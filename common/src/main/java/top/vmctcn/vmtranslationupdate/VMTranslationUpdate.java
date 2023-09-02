bbff45141409bf0132c69041219569aca6d51bfd
TexTrue <3140846162@qq.com>
2023-09-02 17:05:51 +0800

    添加自动切换语言

--- common/src/main/java/top/vmctcn/vmtranslationupdate/VMTranslationUpdate.java
+++ common/src/main/java/top/vmctcn/vmtranslationupdate/VMTranslationUpdate.java
@@ -3,6 +3,7 @@ package top.vmctcn.vmtranslationupdate;
 import dev.architectury.event.events.client.ClientTickEvent;
 import dev.architectury.event.events.common.PlayerEvent;
 
+import dev.architectury.platform.Platform;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.*;
 import net.minecraft.util.Formatting;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import top.vmctcn.vmtranslationupdate.util.*;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
@@ -21,10 +23,15 @@ public class VMTranslationUpdate {
     public static final String MODNAME = "VMTranslationUpdate";
     public static final String MOD_ID = "vmtranslationupdate";
     static MinecraftClient client = MinecraftClient.getInstance();
-
+    // Stenographer 兼容
+    private static final boolean isStenographerLoaded = Platform.isModLoaded("stenographer");
     public static final Logger LOGGER = LoggerFactory.getLogger(MODNAME);
 
     public static void init() {
+        if (ModConfigUtil.getConfig().autoSwitchLanguage && isStenographerLoaded) {
+            client.options.language = (Locale.getDefault().getLanguage() + "_" + Locale.getDefault().getCountry()).toLowerCase();
+        }
+
         random = new Random();
         PackDownloadUtil.downloadResPack();
         ClientTickEvent.CLIENT_POST.register((client) -> {
