e2482946de48571452bcf35b38c951abe36a0e27
3093FengMing <83573615+3093FengMing@users.noreply.github.com>
2023-11-19 21:55:15 +0800

    Update VMTranslationUpdate.java

--- common/src/main/java/top/vmctcn/vmtranslationupdate/VMTranslationUpdate.java
+++ common/src/main/java/top/vmctcn/vmtranslationupdate/VMTranslationUpdate.java
@@ -12,7 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.vmctcn.vmtranslationupdate.util.*;
 
-import java.io.File;
+import java.io.Files;
 import java.util.Locale;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
