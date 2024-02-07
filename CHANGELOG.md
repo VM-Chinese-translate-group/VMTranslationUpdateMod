d6bd25d651128a69f7292eaafa60268d74343278
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-02-07 20:05:41 +0800

    修复自动切换语言的问题

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,5 @@
 ## Changelog
-- Add mod release workflow
-- Clean code
-- fix config typo
-- Update Forge Fabric and Fabric API
+- fix switch lang issue
 
 ## 更新日志
-- github仓库添加模组release workflow
-- 代码清理
-- 修复config拼写错误
-- 更新Forge Fabric和Fabric API版本
+- 修复自动切换语言的问题
--- common/src/main/java/top/vmctcn/vmtranslationupdate/VMTranslationUpdate.java
+++ common/src/main/java/top/vmctcn/vmtranslationupdate/VMTranslationUpdate.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import top.vmctcn.vmtranslationupdate.util.*;
 
 import java.nio.file.Files;
-import java.util.Locale;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
@@ -27,8 +26,8 @@ public class VMTranslationUpdate {
     private static final boolean isStenographerLoaded = Platform.isModLoaded("stenographer"); // Stenographer 兼容
 
     public static void init() {
-        if (ModConfigUtil.getConfig().autoSwitchLanguage && !isStenographerLoaded) {
-            client.options.language = (Locale.getDefault().getLanguage() + "_" + Locale.getDefault().getCountry()).toLowerCase();
+        if (ModConfigUtil.getConfig().autoSwitchLanguage && !isStenographerLoaded && ModConfigUtil.getConfig().switchLanguage != null) {
+            client.options.language = ModConfigUtil.getConfig().switchLanguage;
         }
 
         if (ModConfigUtil.getConfig().autoDownloadVMTranslationPack) {
