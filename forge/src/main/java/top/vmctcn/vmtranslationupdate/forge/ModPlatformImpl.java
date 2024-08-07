d7e371152f28e04ba6f5363f0ba79ffc6a68b3d5
TexTrue <3140846162@qq.com>
2024-08-07 14:57:05 +0800

    rework build script

--- forge/src/main/java/top/vmctcn/vmtranslationupdate/forge/ModPlatformImpl.java
+++
@@ -1,9 +0,0 @@
-package top.vmctcn.vmtranslationupdate.forge;
-
-import net.minecraftforge.fml.ModList;
-
-public class ModPlatformImpl {
-    public static boolean isModLoaded(String modid) {
-        return ModList.get().isLoaded(modid);
-    }
-}
