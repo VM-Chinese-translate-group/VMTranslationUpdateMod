fb3a5cc57ad905bd7550c64a2158866eaa6dfe8c
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-01-11 19:09:52 +0800

    backport to 1.21.1

--- common/src/main/java/top/vmctcn/vmtranslationupdate/verlayer/ScreenHelper.java
+++ common/src/main/java/top/vmctcn/vmtranslationupdate/verlayer/ScreenHelper.java
@@ -1,16 +1,14 @@
 package top.vmctcn.vmtranslationupdate.verlayer;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
 
 public class ScreenHelper {
     public static void drawGuiTexture(DrawContext context, Identifier sprite, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight) {
-        context.drawTexture(RenderLayer::getGuiTextured, sprite, x, y, u, v, width, height, textureWidth, textureHeight);
+        context.drawTexture(sprite, x, y, u, v, width, height, textureWidth, textureHeight);
     }
 
     public static void resetShaderColor(DrawContext context) {
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        context.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
