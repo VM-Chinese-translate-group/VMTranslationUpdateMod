bdd6db4227c4c85850f2688fc1d99b303b59432e
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-01-14 18:19:48 +0800

    clean

--- fabric/src/main/java/top/vmctcn/vmtranslationupdate/fabric/VMTranslationUpdateClientFabric.java
+++ fabric/src/main/java/top/vmctcn/vmtranslationupdate/fabric/VMTranslationUpdateClientFabric.java
@@ -2,13 +2,17 @@ package top.vmctcn.vmtranslationupdate.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import top.vmctcn.vmtranslationupdate.ModEvents;
 import top.vmctcn.vmtranslationupdate.VMTranslationUpdate;
+import top.vmctcn.vmtranslationupdate.respack.GameOptionsSetter;
 
 public class VMTranslationUpdateClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         VMTranslationUpdate.init();
+
+        GameOptionsSetter.init(FabricLoader.getInstance().getGameDir());
 
         ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
             ModEvents.screenAfterInitEvent(screen);
