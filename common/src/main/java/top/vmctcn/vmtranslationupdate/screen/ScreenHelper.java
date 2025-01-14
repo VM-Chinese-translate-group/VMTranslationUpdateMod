9ba6bdfd87ce84139cbad6b32a6c820074200766
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-01-14 22:24:09 +0800

    refactor(screen): 抽离绘制中心文本的逻辑到 ScreenHelper 类
    - 在 ScreenHelper 类中添加 drawCenteredTextWithShadow静态方法
    - 修改 SuggestModScreen 类，使用新的静态方法绘制中心文本

--- common/src/main/java/top/vmctcn/vmtranslationupdate/screen/ScreenHelper.java
+++ common/src/main/java/top/vmctcn/vmtranslationupdate/screen/ScreenHelper.java
@@ -1,13 +1,19 @@
 package top.vmctcn.vmtranslationupdate.screen;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ConfirmLinkScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
 import net.minecraft.util.Util;
 import org.apache.commons.lang3.StringUtils;
 
 public class ScreenHelper {
+    public static void drawCenteredTextWithShadow(DrawContext context, TextRenderer textRenderer, Text text, int centerX, int y, int color) {
+        context.drawCenteredTextWithShadow(textRenderer, text, centerX, y, color);
+    }
+
     public static void drawGuiTexture(DrawContext context, int startX, int startY, int endX, int endY, int z, int colorStart, int colorEnd) {
         context.fillGradient(startX, startY, endX, endY, z, colorStart, colorEnd);
     }
