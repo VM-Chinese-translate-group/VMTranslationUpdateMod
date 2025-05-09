2da7700352967395a1c79f5f8514c3961cba8dfb
Wulian233 <1055917385@qq.com>
2025-05-09 20:37:11 +0800

    发布3.1.0

--- common/src/main/java/top/vmctcn/vmtranslationupdate/modpack/VersionChecker.java
+++ common/src/main/java/top/vmctcn/vmtranslationupdate/modpack/VersionChecker.java
@@ -4,17 +4,17 @@ import top.vmctcn.vmtranslationupdate.VMTranslationUpdate;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URI;
+import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 
 public class VersionChecker {
     public static String getOnlineVersion() {
         try {
-            URI uri = URI.create(ModpackInfoReader.getModpackInfo().getModpack().getTranslation().getUpdateCheckUrl());
-            URLConnection connection = uri.toURL().openConnection();
+            URL url = new URL(ModpackInfoReader.getModpackInfo().getModpack().getTranslation().getUpdateCheckUrl());
+            URLConnection connection = url.openConnection();
 
-            String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0";
+            String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0 MCMod/VmUpdate";
             connection.setRequestProperty("User-Agent", userAgent);
             connection.setConnectTimeout(10000);
 
