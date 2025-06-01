55697f717388a2c87dd8eec10c0b16ca509080aa
Wulian233 <1055917385@qq.com>
2025-06-01 15:26:24 +0800

    跨版本兼容1.21.4/5屏幕，还剩下点击事件的跨版本兼容

--- common/src/main/java/top/vmctcn/vmtranslationupdate/screen/SuggestModScreen.java
+++ common/src/main/java/top/vmctcn/vmtranslationupdate/screen/SuggestModScreen.java
@@ -1,6 +1,5 @@
 package top.vmctcn.vmtranslationupdate.screen;
 
-import com.mojang.blaze3d.opengl.GlStateManager;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
@@ -49,7 +48,7 @@ public class SuggestModScreen extends Screen {
         super.renderBackground(context, mouseX, mouseY, partial);
 
         //Render header and footer separators
-        GlStateManager._enableBlend();
+        GlStateManagerHelper.enableBlend();
         ScreenHelper.resetShaderColor();
         Identifier headerIdentifier = MinecraftClient.getInstance().world == null ? Screen.HEADER_SEPARATOR_TEXTURE : Screen.INWORLD_HEADER_SEPARATOR_TEXTURE;
         Identifier footerIdentifier = MinecraftClient.getInstance().world == null ? Screen.FOOTER_SEPARATOR_TEXTURE : Screen.INWORLD_FOOTER_SEPARATOR_TEXTURE;
