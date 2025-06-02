7e8c6447576c578b0a631427bb70714b5e9fae96
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-06-02 14:51:13 +0800

    分离VMTUCore, 重构部分代码

---
+++ fabric/src/main/java/top/vmctcn/vmtu/mod/fabric/ModPlatformImpl.java
@@ -0,0 +1,39 @@
+package top.vmctcn.vmtu.mod.fabric;
+
+import com.google.auto.service.AutoService;
+import net.fabricmc.loader.api.FabricLoader;
+import top.vmctcn.vmtu.core.util.Reflection;
+import top.vmctcn.vmtu.mod.ModPlatform;
+
+import java.nio.file.Path;
+
+@AutoService(ModPlatform.class)
+public class ModPlatformImpl implements ModPlatform {
+    @Override
+    public String getGameVersion() {
+        try {
+            // Fabric
+            return (String) Reflection.clazz("net.fabricmc.loader.impl.FabricLoaderImpl")
+                    .get("INSTANCE")
+                    .get("getGameProvider()")
+                    .get("getNormalizedGameVersion()").get();
+        } catch (Exception ignored) {
+
+        }
+        try {
+            // Quilt
+            return (String) Reflection.clazz("org.quiltmc.loader.impl.QuiltLoaderImpl")
+                    .get("INSTANCE")
+                    .get("getGameProvider()")
+                    .get("getNormalizedGameVersion()").get();
+        } catch (Exception ignored) {
+
+        }
+        return null;
+    }
+
+    @Override
+    public Path getGameDir() {
+        return FabricLoader.getInstance().getGameDir();
+    }
+}
