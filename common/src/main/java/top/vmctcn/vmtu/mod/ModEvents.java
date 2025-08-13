56a7420ec60d0540460c25c0cbb1797b0c3f665e
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-08-13 20:20:57 +0800

    4.0.0

--- common/src/main/java/top/vmctcn/vmtu/mod/ModEvents.java
+++ common/src/main/java/top/vmctcn/vmtu/mod/ModEvents.java
@@ -3,48 +3,73 @@ package top.vmctcn.vmtu.mod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.client.resource.language.LanguageManager;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import top.vmctcn.vmtu.mod.config.ModConfigHelper;
 import top.vmctcn.vmtu.mod.helper.GameEventHelper;
-import top.vmctcn.vmtu.mod.modpack.ModpackInfo;
-import top.vmctcn.vmtu.mod.modpack.ModpackInfoReader;
+import top.vmctcn.vmtu.mod.helper.LanguageHelper;
+import top.vmctcn.vmtu.mod.modpack.OnlineVersion;
+import top.vmctcn.vmtu.mod.modpack.info.ModpackInfo;
+import top.vmctcn.vmtu.mod.modpack.info.ModpackInfoReader;
 import top.vmctcn.vmtu.mod.modpack.VersionChecker;
 import top.vmctcn.vmtu.mod.screen.SuggestModScreen;
 
 public class ModEvents {
     public static boolean firstTitleScreenShown = false;
 
-    public static void playerJoinEvent(ServerPlayerEntity player) {
-        String localVersion = ModpackInfoReader.getModpackInfo().getModpack().getTranslation().getVersion();
-        String onlineVersion = VersionChecker.getOnlineVersion();
+    public static void playerJoinEvent(PlayerEntity player) {
+        if (player == null) return;
+
+        LanguageManager languageManager = MinecraftClient.getInstance().getLanguageManager();
+
+        ModpackInfo.Modpack modpack = ModpackInfoReader.getModpackInfo().getModpack();
+        ModpackInfo.Translation translation = modpack.getTranslation();
+
+        String localTranslationVersion = translation.getVersion();
+        String localModpackVersion = modpack.getVersion();
+
+        OnlineVersion onlineVersion = VersionChecker.getOnlineVersion();
 
         if (ModConfigHelper.getConfig().testMode) {
-            ModpackInfo.Modpack modpack = ModpackInfoReader.getModpackInfo().getModpack();
-            ModpackInfo.Translation translation = modpack.getTranslation();
-
             player.sendMessage(Text.literal("==================== VMTU testMode ===================="), false);
             player.sendMessage(Text.literal("Modpack Name: " + modpack.getName()), false);
             player.sendMessage(Text.literal("Modpack Version: " + modpack.getVersion()), false);
             player.sendMessage(Text.literal("Modpack Translation URL:§b " + translation.getUrl()), false);
-            player.sendMessage(Text.literal("Modpack Translation Update Check URL:§b " + translation.getUpdateCheckUrl()), false);
+            if (translation.getUpdateCheckUrl() != null) {
+                player.sendMessage(Text.literal("Modpack Translation Update Check URL:§b " + translation.getUpdateCheckUrl()), false);
+            } else {
+                player.sendMessage(Text.literal("Modpack Translation Update Check URL:§b Used vm-meta v2, deprecated this"), false);
+            }
             player.sendMessage(Text.literal("Modpack Translation Language: " + translation.getLanguage()), false);
             player.sendMessage(Text.literal("Modpack Translation Version: " + translation.getVersion()), false);
             player.sendMessage(Text.literal("Modpack Translation Resource Pack Name: " + translation.getResourcePackName()), false);
+            player.sendMessage(Text.literal("Online Translation Version: " + onlineVersion.translationVersion()), false);
+            player.sendMessage(Text.literal("Online Modpack Version: " + onlineVersion.modpackVersion()), false);
+            player.sendMessage(Text.literal("======================================================="), false);
+        }
+
+        if (!translation.getLanguage().equals(languageManager.getLanguage()) && LanguageHelper.isChineseLanguage()) {
+            player.sendMessage(Text.translatable("vmtranslationupdate.message.not_support", translation.getLanguage()), false);
         }
 
         if (ModConfigHelper.getConfig().checkModPackTranslationUpdate) {
-            if (onlineVersion.isEmpty()) {
+            if (!onlineVersion.isValid()) {
                 player.sendMessage(Text.translatable("vmtranslationupdate.message.error"), false);
                 VMTranslationUpdate.LOGGER.warn("Error fetching modpack translation version");
                 return;
             }
 
-            if (!localVersion.equals(onlineVersion)) {
-                String updateUrl = ModpackInfoReader.getModpackInfo().getModpack().getTranslation().getUrl();
-                player.sendMessage(Text.translatable("vmtranslationupdate.message.update", localVersion, onlineVersion));
+            boolean translationUpdateNeeded = !localTranslationVersion.equals(onlineVersion.translationVersion());
+            boolean modpackUpdateNeeded = !onlineVersion.modpackVersion().isEmpty() && !localModpackVersion.equals(onlineVersion.modpackVersion());
+            Text coloredLocalVer = Text.literal(localTranslationVersion).styled(s -> s.withColor(Formatting.YELLOW));
+            Text coloredOnlineVer = Text.literal(onlineVersion.translationVersion()).styled(s -> s.withColor(Formatting.YELLOW));
+
+            if (translationUpdateNeeded) {
+                player.sendMessage(Text.translatable("vmtranslationupdate.message.update", coloredLocalVer, coloredOnlineVer), false);
+                String updateUrl = translation.getUrl();
                 Text message = Text.translatable("vmtranslationupdate.message.update2")
                         .append(Text.translatable(updateUrl)
                                 .setStyle(Style.EMPTY
@@ -53,7 +78,14 @@ public class ModEvents {
                                         .withColor(Formatting.AQUA)
                                 ))
                         .append(Text.translatable("vmtranslationupdate.message.update3"));
-                player.sendMessage(message);
+                player.sendMessage(message, false);
+
+                if (modpackUpdateNeeded){
+                    Text coloredLocalModpackVer = Text.literal(localModpackVersion).styled(s -> s.withColor(Formatting.YELLOW));
+                    Text coloredOnlineModpackVer = Text.literal(onlineVersion.modpackVersion()).styled(s -> s.withColor(Formatting.YELLOW));
+                    player.sendMessage(Text.translatable("vmtranslationupdate.message.update_modpack"), false);
+                    player.sendMessage(Text.translatable("vmtranslationupdate.message.update_modpack_hint", coloredLocalModpackVer, coloredOnlineModpackVer), false);
+                }
             }
         }
     }
@@ -63,16 +95,12 @@ public class ModEvents {
             return;
         }
 
-        String language = MinecraftClient.getInstance().getLanguageManager().getLanguage();
+        boolean needI18n = ModConfigHelper.getConfig().i18nUpdateModCheck && !SuggestModScreen.i18nUpdateModPresent;
+        boolean needVP = ModConfigHelper.getConfig().vaultPatcherCheck && !SuggestModScreen.vaultPatcherPresent;
 
-        if ("zh_cn".equals(language)) {
-            boolean needI18n = ModConfigHelper.getConfig().i18nUpdateModCheck && !SuggestModScreen.i18nUpdateModPresent;
-            boolean needVP = ModConfigHelper.getConfig().vaultPatcherCheck && !SuggestModScreen.vaultPatcherPresent;
-
-            // 只要有任何一个模组需要提示，就显示屏幕
-            if (needI18n || needVP) {
-                MinecraftClient.getInstance().setScreen(new SuggestModScreen(screen));
-            }
+        // 只要有任何一个模组需要提示，就显示屏幕
+        if (needI18n || needVP) {
+            MinecraftClient.getInstance().setScreen(new SuggestModScreen(screen));
         }
 
         firstTitleScreenShown = true;
--- neoforge/src/main/java/top/vmctcn/vmtu/mod/neoforge/VMTranslationUpdateClientNeoForge.java
+++ neoforge/src/main/java/top/vmctcn/vmtu/mod/neoforge/VMTranslationUpdateClientNeoForge.java
@@ -1,18 +1,20 @@
 package top.vmctcn.vmtu.mod.neoforge;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraft.server.network.ServerPlayerEntity;
+import com.mojang.brigadier.Command;
+import net.minecraft.server.command.CommandManager;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
 import net.neoforged.neoforge.client.event.ScreenEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import top.vmctcn.vmtu.mod.ModEvents;
 import top.vmctcn.vmtu.mod.VMTranslationUpdate;
-import top.vmctcn.vmtu.mod.config.ModConfigs;
+import top.vmctcn.vmtu.mod.config.ModConfigHelper;
+import top.vmctcn.vmtu.mod.helper.LanguageHelper;
 import top.vmctcn.vmtu.mod.options.GameOptionsSetter;
 
 @Mod(value = VMTranslationUpdate.MOD_ID, dist = Dist.CLIENT)
@@ -23,15 +25,28 @@ public class VMTranslationUpdateClientNeoForge {
 
             GameOptionsSetter.init(FMLPaths.GAMEDIR.get());
 
-            NeoHelper.registerConfigScreen(modContainer, screen -> AutoConfig.getConfigScreen(ModConfigs.class, screen).get());
+            NeoHelper.registerConfigScreen(modContainer, ModConfigHelper::setConfigScreen);
 
             NeoForge.EVENT_BUS.addListener(PlayerEvent.PlayerLoggedInEvent.class, event -> {
-                ModEvents.playerJoinEvent((ServerPlayerEntity) event.getEntity());
+                if (LanguageHelper.isChineseLanguage()) {
+                    ModEvents.playerJoinEvent(event.getEntity());
+                }
+            });
+            NeoForge.EVENT_BUS.addListener(ScreenEvent.Init.Pre.class, event -> {
+                if (LanguageHelper.isChineseLanguage()) {
+                    ModEvents.screenAfterInitEvent(event.getScreen());
+                }
             });
 
-            NeoForge.EVENT_BUS.addListener(ScreenEvent.Init.Pre.class, event -> {
-                ModEvents.screenAfterInitEvent(event.getScreen());
-            });
+            NeoForge.EVENT_BUS.addListener(RegisterClientCommandsEvent.class, event -> event.getDispatcher().register(
+                    CommandManager.literal("vmtu")
+                            .then(CommandManager.literal("check")
+                                    .executes(context -> {
+                                        ModEvents.playerJoinEvent(context.getSource().getPlayer());
+                                        return Command.SINGLE_SUCCESS;
+                                    })
+                            )
+            ));
         }
     }
 }
