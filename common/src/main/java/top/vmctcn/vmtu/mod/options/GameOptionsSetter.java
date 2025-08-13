fe76a2c10dc9c30959a376a04408ba4362840c15
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-08-13 20:45:45 +0800

    4.0.0

--- common/src/main/java/top/vmctcn/vmtu/mod/options/GameOptionsSetter.java
+++ common/src/main/java/top/vmctcn/vmtu/mod/options/GameOptionsSetter.java
@@ -4,7 +4,7 @@ import top.vmctcn.vmtu.mod.ModPlatform;
 import top.vmctcn.vmtu.mod.VMTranslationUpdate;
 import top.vmctcn.vmtu.mod.config.ModConfigHelper;
 import top.vmctcn.vmtu.mod.helper.LanguageHelper;
-import top.vmctcn.vmtu.mod.modpack.ModpackInfoReader;
+import top.vmctcn.vmtu.mod.modpack.info.ModpackInfoReader;
 import top.vmctcn.vmtu.core.VMTUCore;
 import top.vmctcn.vmtu.core.pack.GameOptionsWriter;
 import top.vmctcn.vmtu.core.pack.PackSource;
