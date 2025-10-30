587bf12de992597d611262b7fc035afb4a9075f0
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-10-31 00:47:24 +0800

    4.1.0

--- neoforge/src/main/java/top/vmctcn/vmtu/mod/neoforge/VMTranslationUpdateClientNeoForge.java
+++ neoforge/src/main/java/top/vmctcn/vmtu/mod/neoforge/VMTranslationUpdateClientNeoForge.java
@@ -23,8 +23,6 @@ public class VMTranslationUpdateClientNeoForge {
         if (FMLLoader.getDist().isClient()) {
             VMTranslationUpdate.init();
 
-            GameOptionsSetter.init(FMLPaths.GAMEDIR.get());
-
             NeoHelper.registerConfigScreen(modContainer, ModConfigHelper::setConfigScreen);
 
             NeoForge.EVENT_BUS.addListener(PlayerEvent.PlayerLoggedInEvent.class, event -> {
