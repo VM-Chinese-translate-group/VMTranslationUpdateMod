0e86384383f96c00af42c49cbd7b819790b040a9
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-10-30 23:13:44 +0800

    1.21.10/9 support

--- neoforge/src/main/java/top/vmctcn/vmtu/mod/neoforge/ModPlatformImpl.java
+++ neoforge/src/main/java/top/vmctcn/vmtu/mod/neoforge/ModPlatformImpl.java
@@ -1,45 +1,17 @@
 package top.vmctcn.vmtu.mod.neoforge;
 
 import com.google.auto.service.AutoService;
-import com.google.gson.JsonObject;
-import cpw.mods.modlauncher.Launcher;
+import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
-import top.vmctcn.vmtu.core.VMTUCore;
-import top.vmctcn.vmtu.core.util.Reflection;
 import top.vmctcn.vmtu.mod.ModPlatform;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Path;
 
 @AutoService(ModPlatform.class)
 public class ModPlatformImpl implements ModPlatform {
     @Override
     public String getGameVersion() {
-        // MinecraftForge 1.13~1.20.2
-        // NeoForge 1.20.1~
-        try {
-            String[] args = (String[]) Reflection.clazz(Launcher.INSTANCE).get("argumentHandler").get("args").get();
-            for (int i = 0; i < args.length - 1; ++i) {
-                if (args[i].equalsIgnoreCase("--fml.mcversion")) {
-                    return args[i + 1];
-                }
-            }
-        } catch (Exception e) {
-            VMTUCore.LOGGER.warn("Error getting minecraft version: %s", e);
-        }
-
-        // MinecraftForge 1.20.3~
-        // 1.20.3: https://github.com/MinecraftForge/MinecraftForge/blob/1.20.x/fmlloader/src/main/java/net/minecraftforge/fml/loading/VersionInfo.java
-        try {
-            Class<?> clazz = Class.forName("net.minecraftforge.fml.loading.FMLLoader");
-            try (InputStream is = clazz.getResourceAsStream("/forge_version.json")) {
-                return VMTUCore.GSON.fromJson(new InputStreamReader(is), JsonObject.class).get("mc").getAsString();
-            }
-        } catch (Exception e) {
-            VMTUCore.LOGGER.warn("Error getting minecraft version: %s", e);
-        }
-        return null;
+        return FMLLoader.getCurrent().getVersionInfo().mcVersion();
     }
 
     @Override
