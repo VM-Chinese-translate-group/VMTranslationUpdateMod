0b8927dbeacb09bd37594c981d47860d6fafdf11
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-10-31 22:32:44 +0800

    fix

--- common/src/main/java/top/vmctcn/vmtu/mod/VMTranslationUpdate.java
+++ common/src/main/java/top/vmctcn/vmtu/mod/VMTranslationUpdate.java
@@ -7,12 +7,15 @@ import top.vmctcn.vmtu.mod.modpack.info.ModpackInfo;
 import top.vmctcn.vmtu.mod.modpack.info.ModpackInfoReader;
 import top.vmctcn.vmtu.mod.modpack.meta.Metadata;
 import top.vmctcn.vmtu.mod.modpack.meta.MetadataReader;
+import top.vmctcn.vmtu.mod.options.GameOptionsSetter;
 
 public class VMTranslationUpdate {
     public static final String MOD_ID = "vmtranslationupdate";
     public static final Logger LOGGER = LoggerFactory.getLogger("VMTranslationUpdateMod");
 
     public static void init() {
+        GameOptionsSetter.autoSwitchLanguage();
+
         if (ModConfigHelper.getConfig().devMode) {
             ModpackInfo.Modpack modpackInfo = ModpackInfoReader.getModpackInfo().getModpack();
             ModpackInfo.Translation translation = modpackInfo.getTranslation();
