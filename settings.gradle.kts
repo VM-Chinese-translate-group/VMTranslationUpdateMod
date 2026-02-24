7d888fa6e19b4afca15d31140bdced5977a4e6de
Wulian233 <1055917385@qq.com>
2026-02-24 10:20:43 +0800

    更新日志

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,6 @@ pluginManagement {
         maven("https://maven.minecraftforge.net")
         maven("https://maven.neoforged.net/releases/")
         maven("https://maven.kikugie.dev/snapshots")
-        maven("https://maven.firstdark.dev/releases")
     }
 }
 
