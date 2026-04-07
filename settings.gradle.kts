923293328e1ad5ffe45738b3f07d6e5d0800aafb
TexTrue <65154269+TexBlock@users.noreply.github.com>
2026-04-07 19:52:59 +0800

    更新依赖

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
 
 val currentStonecutterVersion = file("versions/currentVersion").readText().trim()
 val gradleJava = JavaVersion.current()
-val requiresJava25 = currentStonecutterVersion == "26.1"
+val requiresJava25 = currentStonecutterVersion >= "26.1"
 
 if (requiresJava25 && !gradleJava.isCompatibleWith(JavaVersion.VERSION_25)) {
     error(
@@ -32,18 +32,18 @@ stonecutter {
     kotlinController = true
     //create(rootProject, file("versions/settings.json5"))
     create(rootProject) {
-        vcsVersion = "26.1"
+        vcsVersion = "26.1.1"
         branch("common") {
             versions("1.16.5", "1.18.2", "1.19.2", "1.20.1", "1.20.4", "1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11").buildscript("obfuscated.gradle.kts")
-            versions("26.1")
+            versions("26.1.1")
         }
         branch("fabric") {
             versions("1.16.5", "1.18.2", "1.19.2", "1.20.1", "1.20.4", "1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11").buildscript("obfuscated.gradle.kts")
-            versions("26.1")
+            versions("26.1.1")
         }
         branch("neoforge") {
             versions("1.20.4", "1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11").buildscript("obfuscated.gradle.kts")
-            versions("26.1")
+            versions("26.1.1")
         }
         branch("forge") {
             versions("1.16.5", "1.18.2", "1.19.2", "1.20.1")
