641fd79cae5bb63f8fdc726ae295744344461563
TexTrue <65154269+TexBlock@users.noreply.github.com>
2026-04-11 23:24:29 +0800

    updated to 26.1.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,18 +32,18 @@ stonecutter {
     kotlinController = true
     //create(rootProject, file("versions/settings.json5"))
     create(rootProject) {
-        vcsVersion = "26.1.1"
+        vcsVersion = "26.1.2"
         branch("common") {
             versions("1.16.5", "1.18.2", "1.19.2", "1.20.1", "1.20.4", "1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11").buildscript("obfuscated.gradle.kts")
-            versions("26.1.1")
+            versions("26.1.2")
         }
         branch("fabric") {
             versions("1.16.5", "1.18.2", "1.19.2", "1.20.1", "1.20.4", "1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11").buildscript("obfuscated.gradle.kts")
-            versions("26.1.1")
+            versions("26.1.2")
         }
         branch("neoforge") {
             versions("1.20.4", "1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11").buildscript("obfuscated.gradle.kts")
-            versions("26.1.1")
+            versions("26.1.2")
         }
         branch("forge") {
             versions("1.16.5", "1.18.2", "1.19.2", "1.20.1")
