1c86c253c47907c018925757027677436d242586
Wulian233 <1055917385@qq.com>
2026-08-31 21:03:37 +0800

    Update LanguageManagerMixin.java (#51)

--- fabric/src/main/java/top/vmctcn/vmtu/mod/fabric/mixin/LanguageManagerMixin.java
+++ fabric/src/main/java/top/vmctcn/vmtu/mod/fabric/mixin/LanguageManagerMixin.java
@@ -1,22 +1,20 @@
 package top.vmctcn.vmtu.mod.fabric.mixin;
 
 import com.google.common.collect.Lists;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.resources.language.LanguageInfo;
 import net.minecraft.client.resources.language.LanguageManager;
-import net.minecraft.server.packs.resources.ResourceManager;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import top.vmctcn.vmtu.mod.ModContexts;
 import top.vmctcn.vmtu.mod.utils.LanguageUtils;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Mixin(LanguageManager.class)
@@ -28,43 +26,37 @@ public abstract class LanguageManagerMixin {
     @Nullable
     public abstract LanguageInfo getLanguage(String string);
 
-    @Inject(
+    @ModifyArg(
             method = "onResourceManagerReload",
             at = @At(
                     value = "INVOKE",
-                    /*? if >= 1.20.1 {*/
-                    target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z",
-                    /*?} else {*/
-                    /*target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
-                    *//*?}*/
-                    shift = At.Shift.AFTER
-            )
+                    //? if >= 1.20.1 {
+                    target = "Lnet/minecraft/client/resources/language/ClientLanguage;loadFrom(Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/util/List;Z)Lnet/minecraft/client/resources/language/ClientLanguage;"
+                    //?} else {
+                    /*target = "Lnet/minecraft/client/resources/language/TranslationStorage;load(Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/util/List;)Lnet/minecraft/client/resources/language/TranslationStorage;"*/
+                    //?}
+            ),
+            index = 1
     )
-    private void insertQueue(
-            ResourceManager resourceManager,
-            CallbackInfo ci,
-            @Local(name = /*? if >= 26.1.2 {*/"languageStack"/*?} else {*//*"list"*//*?}*/) List</*? if >= 1.20.1 {*/String/*?} else {*//*LanguageInfo*//*?}*/> languageStack
-    ) {
+    private List</*? if >= 1.20.1 {*/String/*?} else {*//*LanguageInfo*//*?}*/> insertQueue(List</*? if >= 1.20.1 {*/String/*?} else {*//*LanguageInfo*//*?}*/> languageStack) {
         Map<String, String[]> queues = LanguageUtils.getFallbackQueues();
-        if (!queues.containsKey(currentCode)) return;
+        if (!queues.containsKey(currentCode)) {
+            ModContexts.LOGGER.info("Language Loading Order: {}(In reverse order)", languageStack);
+            return languageStack;
+        }
 
         //? if >= 1.20.1 {
-        languageStack.addAll(List.of(queues.get(currentCode)));
+        List<String> result = Lists.newArrayList(languageStack);
+        result.addAll(Math.max(0, result.size() - 1), Arrays.asList(queues.get(currentCode)));
         //?} else {
-        /*List<LanguageInfo> listLanguageInfo = Arrays.stream(queues.get(currentCode))
+        /*List<LanguageInfo> result = Lists.newArrayList(languageStack);
+        result.addAll(Math.max(0, result.size() - 1), Arrays.stream(queues.get(currentCode))
                 .map(this::getLanguage)
-                .collect(Collectors.toCollection(Lists::newArrayList));
-
-        languageStack.addAll(listLanguageInfo);
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList()));
         *///?}
-    }
 
-    @Inject(method = "onResourceManagerReload", at = @At("TAIL"))
-    private void logLoadedQueue(
-            ResourceManager resourceManager,
-            CallbackInfo ci,
-            @Local(name = /*? if >= 26.1.2 {*/"languageStack"/*?} else {*//*"list"*//*?}*/) List</*? if >= 1.20.1 {*/String/*?} else {*//*LanguageInfo*//*?}*/> languageStack
-    ) {
-        ModContexts.LOGGER.info("Language Loading Order: {}(In reverse order)", languageStack);
+        ModContexts.LOGGER.info("Language Loading Order: {}(In reverse order)", result);
+        return result;
     }
 }
